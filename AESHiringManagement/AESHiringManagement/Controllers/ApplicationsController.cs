075e1d942485ee781c28cba1aaa30fa2f09d5d10
KeithVanderzanden <keith.vanderzanden@oit.edu>
2014-05-18 01:31:41 -0700

    Stubs writting for controllers, action methods, and view.  Menus are linked to appropriate actions.

---
+++ AESHiringManagement/AESHiringManagement/Controllers/ApplicationsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AESHiringManagement.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+       
+        public ActionResult Search()
+        {
+            return View();
+        }
+	}
+}
